6f16f491aae8e67a81dafe1dada6c48ee7e4bdca
lcolladotor <lcollado@jhsph.edu>
2014-04-14 20:21:04 -0400

    v0.0.12 Implementing formatting changes after using BiocCheck()

--- R/generateReport.R
+++ R/generateReport.R
@@ -1,31 +1,58 @@
 #' Generate a HTML report exploring the basic results from derfinder
 #'
-#' This function generates a HTML report exploring the basic results from derfinder (https://github.com/lcolladotor/derfinder). The HTML report itself is generated using knitrBoostrap (https://github.com/jimhester/knitrBootstrap) which uses knitr (http://yihui.name/knitr/) behind the scenes. It works best after using \link[derfinder]{mergeResults}.
+#' This function generates a HTML report exploring the basic results from
+#' derfinder (https://github.com/lcolladotor/derfinder). The HTML report itself 
+#' is generated using knitrBoostrap which uses knitr (http://yihui.name/knitr/) 
+#' behind the scenes. It works best after using \link[derfinder]{mergeResults}.
 #' 
-#' @param prefix The main data directory path where \link[derfinder]{mergeResults} was run. It should be the same as \code{mergeResults(prefix)}.
+#' @param prefix The main data directory path where 
+#' \link[derfinder]{mergeResults} was run. It should be the same as 
+#' \code{mergeResults(prefix)}.
 #' @param outdir The name of output directory relative to \code{prefix}.
 #' @param output The name of output HTML file (without the html extension).
 #' @param project The title of the project.
-#' @param browse If \code{TRUE} the HTML report is opened in your browser once it's completed.
+#' @param browse If \code{TRUE} the HTML report is opened in your browser once 
+#' it's completed.
 #' @param nBestRegions The number of region plots to make, ordered by area.
-#' @param makeBestClusters If \code{TRUE}, \link[derfinder]{plotCluster} is used on the \code{nBestClusters} regions by area. Note that these plots take some time to make.
-#' @param nBestClusters The number of region cluster plots to make by taking the \code{nBestClusters} regions ranked by area of the cluster.
-#' @param fullCov A list where each element is the result from \link[derfinder]{loadCoverage} used with \code{cutoff=NULL}. The elements of the list should be named according to the chromosome number. Can be generated using \link{fullCoverage}.
-#' @param hg19 If \code{TRUE} then the reference is assumed to be hg19 and chromosome lengths as well as the default transcription database (TxDb.Hsapiens.UCSC.hg19.knownGene) will be used.
-#' @param p.ideos A list where each element is the result of \link[ggbio]{plotIdeogram}. If it's \code{NULL} and \code{hg19=TRUE} then they are created for the hg19 human reference.
-#' @param txdb Specify the transcription database to use for making the plots for the top regions by area. If \code{NULL} and \code{hg19=TRUE} then TxDb.Hsapiens.UCSC.hg19.knownGene is used.
-#' @param device The graphical device used when knitting. See more at http://yihui.name/knitr/options (dev argument).
-#' @param fullRegions Part of the output of \link[derfinder]{mergeResults}. Specify it only if you have already loaded it in memory.
-#' @param fullNullSummary Part of the output of \link[derfinder]{mergeResults}. Specify it only if you have already loaded it in memory.
-#' @param fullAnnotatedRegions Part of the output of \link[derfinder]{mergeResults}. Specify it only if you have already loaded it in memory.
-#' @param optionsStats Part of the output of \link[derfinder]{analyzeChr}. Specify it only if you have already loaded it in memory.
-#' @param optionsMerge Part of the output of \link[derfinder]{mergeResults}. Specify it only if you have already loaded it in memory.
-#' @param overviewParams A two element list with \code{base_size} and \code{areaRel} that control the text size for the genomic overview plots.
+#' @param makeBestClusters If \code{TRUE}, \link[derfinder]{plotCluster} is 
+#' used on the \code{nBestClusters} regions by area. Note that these plots take 
+#' some time to make.
+#' @param nBestClusters The number of region cluster plots to make by taking 
+#' the \code{nBestClusters} regions ranked by area of the cluster.
+#' @param fullCov A list where each element is the result from 
+#' \link[derfinder]{loadCoverage} used with \code{cutoff=NULL}. The elements of 
+#' the list should be named according to the chromosome number. Can be 
+#' generated using \link{fullCoverage}.
+#' @param hg19 If \code{TRUE} then the reference is assumed to be hg19 and 
+#' chromosome lengths as well as the default transcription database 
+#' (TxDb.Hsapiens.UCSC.hg19.knownGene) will be used.
+#' @param p.ideos A list where each element is the result of 
+#' \link[ggbio]{plotIdeogram}. If it's \code{NULL} and \code{hg19=TRUE} then 
+#' they are created for the hg19 human reference.
+#' @param txdb Specify the transcription database to use for making the plots 
+#' for the top regions by area. If \code{NULL} and \code{hg19=TRUE} then 
+#' TxDb.Hsapiens.UCSC.hg19.knownGene is used.
+#' @param device The graphical device used when knitting. See more at 
+#' http://yihui.name/knitr/options (dev argument).
+#' @param fullRegions Part of the output of \link[derfinder]{mergeResults}. 
+#' Specify it only if you have already loaded it in memory.
+#' @param fullNullSummary Part of the output of \link[derfinder]{mergeResults}. 
+#' Specify it only if you have already loaded it in memory.
+#' @param fullAnnotatedRegions Part of the output of 
+#' \link[derfinder]{mergeResults}. Specify it only if you have already loaded 
+#' it in memory.
+#' @param optionsStats Part of the output of \link[derfinder]{analyzeChr}. 
+#' Specify it only if you have already loaded it in memory.
+#' @param optionsMerge Part of the output of \link[derfinder]{mergeResults}. 
+#' Specify it only if you have already loaded it in memory.
+#' @param overviewParams A two element list with \code{base_size} and 
+#' \code{areaRel} that control the text size for the genomic overview plots.
 #'
 #' @return An HTML report with a basic exploration of the results.
 #'
 #' @author Leonardo Collado-Torres
-#' @seealso \link[derfinder]{mergeResults}, \link[derfinder]{analyzeChr}, \link[derfinder]{fullCoverage}
+#' @seealso \link[derfinder]{mergeResults}, \link[derfinder]{analyzeChr}, 
+#' \link[derfinder]{fullCoverage}
 #' @export
 #'
 #' @import derfinder
@@ -47,20 +74,22 @@
 #' @examples
 #'
 #' ## Load derfinder
-#' library("derfinder")
+#' library('derfinder')
 #'
 #' ## The output will be saved in the 'generateReport-example' directory
-#' dir.create("generateReport-example", showWarnings = FALSE, recursive = TRUE)
+#' dir.create('generateReport-example', showWarnings = FALSE, recursive = TRUE)
 #' initialPath <- getwd()
-#' setwd(file.path(initialPath, "generateReport-example"))
+#' setwd(file.path(initialPath, 'generateReport-example'))
 #' 
 #' ## Generate output from derfinder
 #' 
 #' ## Collapse the coverage information
-#' collapsedFull <- collapseFullCoverage(list(genomeData$coverage), verbose=TRUE)
+#' collapsedFull <- collapseFullCoverage(list(genomeData$coverage), 
+#' verbose=TRUE)
 #' 
 #' ## Calculate library size adjustments
-#' sampleDepths <- sampleDepth(collapsedFull, probs=c(0.5), nonzero=TRUE, verbose=TRUE)
+#' sampleDepths <- sampleDepth(collapsedFull, probs=c(0.5), nonzero=TRUE, 
+#' verbose=TRUE)
 #' 
 #' ## Build the models
 #' group <- genomeInfo$pop
@@ -68,19 +97,25 @@
 #' models <- makeModels(sampleDepths, testvars=group, adjustvars=adjustvars)
 #'
 #' ## Analyze chromosome 21
-#' analyzeChr(chrnum="21", coverageInfo=genomeData, models=models, cutoffFstat=1, cutoffType="manual", seeds=20140330, groupInfo=group, mc.cores=1, writeOutput=TRUE, returnOutput=FALSE)
+#' analyzeChr(chrnum='21', coverageInfo=genomeData, models=models, 
+#' cutoffFstat=1, cutoffType='manual', seeds=20140330, groupInfo=group, 
+#' mc.cores=1, writeOutput=TRUE, returnOutput=FALSE)
 #'
 #' ## Change the directory back to the original one
 #' setwd(initialPath)
 #'
-#' ## Merge the results from the different chromosomes. In this case, there's only one: chr21
-#' mergeResults(chrnums="21", prefix="generateReport-example", genomicState=genomicState)
+#' ## Merge the results from the different chromosomes. In this case, there's 
+#' ## only one: chr21
+#' mergeResults(chrnums='21', prefix='generateReport-example', 
+#' genomicState=genomicState)
 #'
 #' ## Load the options used for calculating the statistics
-#' load(file.path("generateReport-example", "chr21", "optionsStats.Rdata"))
+#' load(file.path('generateReport-example', 'chr21', 'optionsStats.Rdata'))
 #'
 #' ## Generate the HTML report
-#' report <- generateReport(prefix="generateReport-example", browse=FALSE, nBestRegions=15, makeBestClusters=TRUE, fullCov=list("21"=genomeDataRaw$coverage), optionsStats=optionsStats)
+#' report <- generateReport(prefix='generateReport-example', browse=FALSE, 
+#' nBestRegions=15, makeBestClusters=TRUE, 
+#' fullCov=list('21'=genomeDataRaw$coverage), optionsStats=optionsStats)
 #'
 #'
 #' if(interactive()) {
@@ -90,99 +125,116 @@
 #'
 #' \dontrun{
 #' ## Note that you can run the example using:
-#' example("generateReport", "derfinderReport", ask=FALSE)
+#' example('generateReport', 'derfinderReport', ask=FALSE)
 #' }
 
 
 
-generateReport <- function(prefix, outdir="basicExploration", output="basicExploration", project=prefix, browse=interactive(), nBestRegions=100, makeBestClusters=TRUE, nBestClusters=2, fullCov=NULL, hg19=TRUE, p.ideos=NULL, txdb=NULL, device="CairoPNG", fullRegions=NULL, fullNullSummary=NULL, fullAnnotatedRegions=NULL, optionsStats=NULL, optionsMerge=NULL, overviewParams=list(base_size=10, areaRel=5)) {
-
-	## Save start time for getting the total processing time
-	startTime <- Sys.time()
-	
-	## Check that overviewParams is correctly specified
-	stopifnot(sum(names(overviewParams) %in% c("base_size", "areaRel")) == 2)
-
-	if(hg19) {
-		library("biovizBase")
-		library("TxDb.Hsapiens.UCSC.hg19.knownGene")
-	}
-	
-	## Create outdir
-	dir.create(file.path(prefix, outdir), showWarnings = FALSE, recursive = TRUE)
-	workingDir <- file.path(getwd(), prefix)
-	
-	## Locate Rmd
-	template <- system.file(file.path("basicExploration", "basicExploration.Rmd"), package="derfinderReport", mustWork=TRUE)
-	
-	## Load knitcitations with a clean bibliography
-	cleanbib()
-	cite_options(tooltip=TRUE)
-	
-	## Fix citep
-	mycitep <- function(x, short=NULL) {
-		res <- gsub("  </p>", "", citep(x))
-		if(!is.null(short)) {
-			res <- gsub("></a>", paste0(">", short, "</a>"), res)
-		}		
-		return(res)
-	}
-	
-	## Write bibliography information
-	write.bibtex(c("knitcitations" = citation("knitcitations"), "derfinder" = citation("derfinder"), "derfinderReport" = citation("derfinderReport"), "knitrBootstrap" = citation("knitrBootstrap"), "ggbio" = citation("ggbio"), "ggplot2" = citation("ggplot2"), "rCharts" = citation("rCharts"), "knitr" = citation("knitr")[3], "rmarkdown" = citation("rmarkdown")), file = file.path(prefix, outdir, "references.bib"))
-	bib <- read.bibtex(file.path(prefix, outdir, "references.bib"))
-	
-	## Load files
-	if(is.null(fullRegions)) load(file.path(prefix, "fullRegions.Rdata"))
-	if(is.null(fullNullSummary)) load(file.path(prefix, "fullNullSummary.Rdata"))
-	if(is.null(fullAnnotatedRegions)) load(file.path(prefix, "fullAnnotatedRegions.Rdata"))
-	if(is.null(optionsStats)) load(file.path(prefix, dir(prefix, pattern="chr")[1], "optionsStats.Rdata"))
-	if(is.null(optionsMerge)) load(file.path(prefix, "optionsMerge.Rdata"))
-
-	## Require fullCov
-	if(makeBestClusters) {
-		stopifnot(!is.null(fullCov))
-	} else {
-		nBestClusters <- 0
-		nBestRegions <- 0
-	}
-
-	##### Setup chunk options
-	## Are there any null regions? If not, then there won't be any p-values either.
-	nullExist <- length(fullNullSummary) > 0
-	## Were permutations used?
-	seeds <- optionsStats$seeds
-	usedPermutations <- length(optionsStats$nPermute) > 0 & !is.null(seeds)
-	## Are there significant (by q-value) regions?
-	idx.sig <- which(as.logical(fullRegions$significantQval))
-	hasSig <- length(idx.sig) > 0
-	## Are there regions with infite area?
-	finite.area <- which(is.finite(fullRegions$area))
-	if(length(intersect(idx.sig, finite.area)) > 0 ) {
-		hasArea <- TRUE
-		inf.area <- sum(!is.finite(fullRegions$area))
-	} else {
-		hasArea <- FALSE
-		inf.area <- sum(!is.finite(fullRegions$area))
-	}
-	
-	## Save the call
-	theCall <- match.call()
-	
-	## knitrBoostrap chunk options
-	opts_chunk$set(bootstrap.show.code=FALSE)
-	
-	## Generate report
-	tmpdir <- getwd()
-	setwd(file.path(prefix, outdir))
-	file.copy(template, to=paste0(output, ".Rmd"))
-	res <- render(paste0(output, ".Rmd"), bootstrap_document(theme.chooser=TRUE, highlight.chooser=TRUE, highlight="Brown Paper"))
-	file.remove(paste0(output, ".Rmd"))
-	
-	## Open
-	if (browse) browseURL(res)
-	setwd(tmpdir)
-		
-	## Finish
-	return(invisible(res))
-}
+generateReport <- function(prefix, outdir = "basicExploration", output = "basicExploration", 
+    project = prefix, browse = interactive(), nBestRegions = 100, makeBestClusters = TRUE, 
+    nBestClusters = 2, fullCov = NULL, hg19 = TRUE, p.ideos = NULL, txdb = NULL, 
+    device = "CairoPNG", fullRegions = NULL, fullNullSummary = NULL, fullAnnotatedRegions = NULL, 
+    optionsStats = NULL, optionsMerge = NULL, overviewParams = list(base_size = 10, 
+    areaRel = 5)) {
+    
+    ## Save start time for getting the total processing time
+    startTime <- Sys.time()
+    
+    ## Check that overviewParams is correctly specified
+    stopifnot(sum(names(overviewParams) %in% c("base_size", "areaRel")) == 2)
+    
+    if (hg19) {
+        library("biovizBase")
+        library("TxDb.Hsapiens.UCSC.hg19.knownGene")
+    }
+    
+    ## Create outdir
+    dir.create(file.path(prefix, outdir), showWarnings = FALSE, recursive = TRUE)
+    workingDir <- file.path(getwd(), prefix)
+    
+    ## Locate Rmd
+    template <- system.file(file.path("basicExploration", "basicExploration.Rmd"), 
+        package = "derfinderReport", mustWork = TRUE)
+    
+    ## Load knitcitations with a clean bibliography
+    cleanbib()
+    cite_options(tooltip = TRUE)
+    
+    ## Fix citep
+    mycitep <- function(x, short = NULL) {
+        res <- gsub("  </p>", "", citep(x))
+        if (!is.null(short)) {
+            res <- gsub("></a>", paste0(">", short, "</a>"), res)
+        }
+        return(res)
+    }
+    
+    ## Write bibliography information
+    write.bibtex(c(knitcitations = citation("knitcitations"), derfinder = citation("derfinder"), 
+        derfinderReport = citation("derfinderReport"), knitrBootstrap = citation("knitrBootstrap"), 
+        ggbio = citation("ggbio"), ggplot2 = citation("ggplot2"), rCharts = citation("rCharts"), 
+        knitr = citation("knitr")[3], rmarkdown = citation("rmarkdown")), file = file.path(prefix, 
+            outdir, "references.bib"))
+    bib <- read.bibtex(file.path(prefix, outdir, "references.bib"))
+    
+    ## Load files
+    if (is.null(fullRegions)) 
+        load(file.path(prefix, "fullRegions.Rdata"))
+    if (is.null(fullNullSummary)) 
+        load(file.path(prefix, "fullNullSummary.Rdata"))
+    if (is.null(fullAnnotatedRegions)) 
+        load(file.path(prefix, "fullAnnotatedRegions.Rdata"))
+    if (is.null(optionsStats)) 
+        load(file.path(prefix, dir(prefix, pattern = "chr")[1], "optionsStats.Rdata"))
+    if (is.null(optionsMerge)) 
+        load(file.path(prefix, "optionsMerge.Rdata"))
+    
+    ## Require fullCov
+    if (makeBestClusters) {
+        stopifnot(!is.null(fullCov))
+    } else {
+        nBestClusters <- 0
+        nBestRegions <- 0
+    }
+    
+    ##### Setup chunk options Are there any null regions? If not, then there 
+    ##### won't be any p-values either.
+    nullExist <- length(fullNullSummary) > 0
+    ## Were permutations used?
+    seeds <- optionsStats$seeds
+    usedPermutations <- length(optionsStats$nPermute) > 0 & !is.null(seeds)
+    ## Are there significant (by q-value) regions?
+    idx.sig <- which(as.logical(fullRegions$significantQval))
+    hasSig <- length(idx.sig) > 0
+    ## Are there regions with infite area?
+    finite.area <- which(is.finite(fullRegions$area))
+    if (length(intersect(idx.sig, finite.area)) > 0) {
+        hasArea <- TRUE
+        inf.area <- sum(!is.finite(fullRegions$area))
+    } else {
+        hasArea <- FALSE
+        inf.area <- sum(!is.finite(fullRegions$area))
+    }
+    
+    ## Save the call
+    theCall <- match.call()
+    
+    ## knitrBoostrap chunk options
+    opts_chunk$set(bootstrap.show.code = FALSE)
+    
+    ## Generate report
+    tmpdir <- getwd()
+    setwd(file.path(prefix, outdir))
+    file.copy(template, to = paste0(output, ".Rmd"))
+    res <- render(paste0(output, ".Rmd"), bootstrap_document(theme.chooser = TRUE, 
+        highlight.chooser = TRUE, highlight = "Brown Paper"))
+    file.remove(paste0(output, ".Rmd"))
+    
+    ## Open
+    if (browse) 
+        browseURL(res)
+    setwd(tmpdir)
+    
+    ## Finish
+    return(invisible(res))
+} 
--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 derfinderReport
 ===============
 
-Generate HTML reports for [derfinder](https://github.com/lcolladotor/derfinder) results.
+Generate HTML reports for [derfinder](https://github.com/lcolladotor/derfinder) 
+results.
 
-Check the documentation for _generateReport()_ for an example on how to create the necessary input files and generating the HTML report for __derfinder__ results. Or use:
+Check the documentation for _generateReport()_ for an example on how to create 
+the necessary input files and generating the HTML report for __derfinder__ 
+results. Or use:
 
 ```S
 example("generateReport", "derfinderReport", ask=FALSE)
 ```
 
-For a full example on how to use __derfinder__ and __derfinderReport__ check https://github.com/lcolladotor/derfinderExample.
+For a full example on how to use __derfinder__ and __derfinderReport__ check 
+https://github.com/lcolladotor/derfinderExample.
 
 # Installation instructions
 
-Get R 3.1.0 or newer from [CRAN](http://cran.r-project.org/) as well as pandoc 1.12.3 or newer from [here](http://johnmacfarlane.net/pandoc/installing.html).
+Get R 3.1.0 or newer from [CRAN](http://cran.r-project.org/) as well as pandoc 
+1.12.3 or newer from [here](http://johnmacfarlane.net/pandoc/installing.html).
 
 ```S
 ## If needed
@@ -22,19 +27,20 @@ install.packages("devtools")
 ## Note that knitr 1.5.27 is currently not available via CRAN
 ## Following https://github.com/yihui/knitr
 install.packages('knitr', repos = c('http://rforge.net', 'http://cran.rstudio.org'),
-                 type = 'source')
-				 
+    type = 'source')
+    			 
 ## Pre-requisites from CRAN
 install.packages(c("xtable", "ggplot2", "gridExtra", "data.table", "knitcitations",
-	"RColorBrewer"))
-	
-## You might need to install the Cairo package, but that will depend on the device you use
-## for the plots
+    "RColorBrewer"))
+    
+## You might need to install the Cairo package, but that will depend on the device 
+## you use for the plots
 install.packages("Cairo")
 
 ## Pre-requisites from Bioconductor
 source("http://bioconductor.org/biocLite.R")
-biocLite(c("IRanges", "GenomicRanges", "biovizBase", "ggbio", "TxDb.Hsapiens.UCSC.hg19.knownGene"))
+biocLite(c("IRanges", "GenomicRanges", "biovizBase", "ggbio", 
+    "TxDb.Hsapiens.UCSC.hg19.knownGene"))
 
 ## GitHub dependencies
 library("devtools")
@@ -50,14 +56,26 @@ install_github("derfinderReport", "lcolladotor")
 
 # Citation
 
-Below is the citation output from using `citation("derfinderReport")` in R. Please run this yourself to check for any updates on how to cite __derfinderReport__.
+Below is the citation output from using `citation("derfinderReport")` in R. 
+Please run this yourself to check for any updates on how to cite 
+__derfinderReport__.
 
 ---
 
 To cite package __derfinderReport__ in publications use:
 
-Leonardo Collado-Torres, Andrew Jaffe and Jeffrey Leek (2014). derfinderReport: Generate HTML reports for derfinder (https://github.com/lcolladotor/derfinder) results. R package version 0.0.11. https://github.com/lcolladotor/derfinderReport
+Leonardo Collado-Torres, Andrew Jaffe and Jeffrey Leek (2014). derfinderReport: 
+Generate HTML reports for derfinder (https://github.com/lcolladotor/derfinder) 
+results. R package version 0.0.12. 
+https://github.com/lcolladotor/derfinderReport
 
 A BibTeX entry for LaTeX users is
 
-@Manual{, title = {derfinderReport: Generate HTML reports for derfinder (https://github.com/lcolladotor/derfinder) results}, author = {Leonardo Collado-Torres and Andrew Jaffe and Jeffrey Leek}, year = {2014}, note = {R package version 0.0.11}, url = {https://github.com/lcolladotor/derfinderReport}, }
+@Manual{,
+    title = {derfinderReport: Generate HTML reports for derfinder
+        (https://github.com/lcolladotor/derfinder) results},
+    author = {Leonardo Collado-Torres and Andrew Jaffe and Jeffrey Leek},
+    year = {2014},
+    note = {R package version 0.0.12},
+    url = {https://github.com/lcolladotor/derfinderReport},
+}

7d679f61c675f1d0e252626ed638dd9b1f1aaa70
lcolladotor <lcollado@jhsph.edu>
2014-09-23 23:24:32 -0400

    Fix DESCRIPTION and package man page

--- R/reportReport-package.R
+++ R/reportReport-package.R
@@ -1,14 +1,15 @@
-#' Generate HTML reports for results from fast differential expression analysis of RNA-seq data at base-pair resolution performed by derfinder
+#' Generate HTML reports for a aset of regions.
 #'
-#' Generate an HTML report using the results from derfinder (https://github.com/lcolladotor/derfinder). The HTML report itself is generated using knitrBoostrap (https://github.com/jimhester/knitrBootstrap) which uses knitr (http://yihui.name/knitr/) behind the scenes.
+#' Generate an HTML reports to explore a set of regions such as the
+#' results from annotation-agnostic expression analysis of RNA-seq data at 
+#' base-pair resolution performed by derfinder.
+#' The HTML report itself is generated using knitrBootstrap 
+#' (https://github.com/jimhester/knitrBootstrap).
 #'
-#' @name derfinder-package
-#' @aliases derfinder-package
+#' @name regionReport-package
+#' @aliases regionReport-package
 #' @docType package
 #' @author Leonardo Collado-Torres <lcollado@@jhsph.edu>
-#' @references 
-#'
-#' Leonardo Collado-Torres, Alyssa Frazee, Andrew Jaffe and Jeffrey Leek (2013). derfinder: Fast differential expression analysis of RNA-seq data at base-pair resolution. https://github.com/lcolladotor/derfinder
 #'
 #' @keywords package
 NULL

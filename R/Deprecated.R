a31543945689ab97928a117196b7c62cec552c08
l.collado-torres <l.collado-torres@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-13 14:25:29 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 4721c0198adb819c546074427215a4df9a684a32
    
        v1.3.2 -- deprecated underscore functions
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/regionReport@104918 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/Deprecated.R
@@ -0,0 +1,24 @@
+#' Deprecated functions in package \sQuote{regionReport}
+#'
+#' These functions are provided for compatibility with older versions
+#' of \sQuote{regionReport} only, and will be defunct at the next release.
+#'
+#' @details 
+#'
+#' The following functions are deprecated and will be made defunct; use
+#'  the replacement indicated below:
+#'  \itemize{
+#'
+#'    \item{plot_cluster: \code{\link{plotCluster}}}
+#'    \item{render_report: \code{\link{render_report}}}
+#'
+#'  }
+#'
+#' @name regionReport-deprecated
+#' @aliases regionReport-deprecated
+
+#' @export
+derfinder_report <- function() { .Deprecated('derfinderReport')}
+
+#' @export
+render_report <- function() { .Deprecated('renderReport')}

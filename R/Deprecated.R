30c7acb5bb40479bdba8550f91a210b0bb411f55
lcolladotor <lcollado@jhsph.edu>
2015-06-15 09:41:16 -0400

    Fix deprecated functions warnings

--- R/Deprecated.R
+++ R/Deprecated.R
@@ -20,5 +20,21 @@
 #' @export
 derfinder_report <- function() { .Deprecated('derfinderReport')}
 
+#' Deprecated functions in package \sQuote{regionReport}
+#'
+#' These functions are provided for compatibility with older versions
+#' of \sQuote{regionReport} only, and will be defunct at the next release.
+#'
+#' @details 
+#'
+#' The following functions are deprecated and will be made defunct; use
+#'  the replacement indicated below:
+#'  \itemize{
+#'
+#'    \item{plot_cluster: \code{\link{plotCluster}}}
+#'    \item{render_report: \code{\link{render_report}}}
+#'
+#'  }
+#'
 #' @export
 render_report <- function() { .Deprecated('renderReport')}

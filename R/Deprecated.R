abd1760d24e9c6d2f3a424e022ac816416c4fa93
l.collado-torres <l.collado-torres@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-16 18:33:43 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: e9c5030d02ab1a43fab7e24e908c5d9bc0cb6cc8
    
        Simplify roxygen for deprecated funs, reduce man
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/regionReport@105027 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Deprecated.R
+++ R/Deprecated.R
@@ -15,26 +15,9 @@
 #'  }
 #'
 #' @name regionReport-deprecated
-#' @aliases regionReport-deprecated
-
 #' @export
 derfinder_report <- function() { .Deprecated('derfinderReport')}
 
-#' Deprecated functions in package \sQuote{regionReport}
-#'
-#' These functions are provided for compatibility with older versions
-#' of \sQuote{regionReport} only, and will be defunct at the next release.
-#'
-#' @details 
-#'
-#' The following functions are deprecated and will be made defunct; use
-#'  the replacement indicated below:
-#'  \itemize{
-#'
-#'    \item{plot_cluster: \code{\link{plotCluster}}}
-#'    \item{render_report: \code{\link{render_report}}}
-#'
-#'  }
-#'
+#' @rdname regionReport-deprecated
 #' @export
 render_report <- function() { .Deprecated('renderReport')}

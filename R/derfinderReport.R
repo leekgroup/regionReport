leekgroup/regionReport

commit f7a4d66a2279f771d30da23d3f8d22428ece1484

v1.3.5 -- Finished merging changes from David Robinson
Added 'customCode' and 'template' arguments to derfinderReport() to match the
behavior of renderReport().

R/derfinderReport.R
@@ -37,6 +37,13 @@
 #' p-values, the FDR adjusted p-values or the FWER adjusted p-values to
 #' determine significance. Has to be either \code{'pvalue'}, \code{'qvalue'}
 #' or \code{'fwer'}.
+#' @param customCode An absolute path to a child R Markdown file with code to be
+#' evaluated before the reproducibility section. Its useful for users who want
+#' to customize the report by adding conclusions derived from the data and/or
+#' further quality checks and plots.
+#' @param template Template file to use for the report. If not provided, will
+#' use the default file found in basicExploration/basicExploration.Rmd
+#' within the package source.
 #' @param ... Arguments passed to other methods and/or advanced arguments.
 #'
 #' @return An HTML report with a basic exploration of the derfinder results.
@@ -132,10 +139,14 @@ derfinderReport <- function(prefix, outdir = 'basicExploration',
     output = 'basicExploration', project = prefix, browse = interactive(),
     nBestRegions = 100, makeBestClusters = TRUE, nBestClusters = 2, 
     fullCov = NULL, hg19 = TRUE, p.ideos = NULL, txdb = NULL, 
-    device = 'CairoPNG', significantVar = 'qvalue', ...) {
+    device = 'CairoPNG', significantVar = 'qvalue', customCode = NULL,
+    template = NULL, ...) {
     
     stopifnot(length(significantVar) == 1)
     stopifnot(significantVar %in% c('pvalue', 'qvalue', 'fwer'))
+    
+    hasCustomCode <- !is.null(customCode)
+    if(hasCustomCode) stopifnot(length(customCode) == 1)
     
     ## Save start time for getting the total processing time
     startTime <- Sys.time()
@@ -200,9 +211,13 @@ derfinderReport <- function(prefix, outdir = 'basicExploration',
         recursive = TRUE)
     workingDir <- file.path(getwd(), prefix)
     
-    ## Locate Rmd
-    template <- system.file(file.path('basicExploration',
-        'basicExploration.Rmd'), package = 'regionReport', mustWork = TRUE)
+    ## Locate Rmd if one is not provided
+    if (is.null(template)) {
+        template <- system.file(
+            file.path('basicExploration', 'basicExploration.Rmd'),
+            package = 'regionReport', mustWork = TRUE
+        )
+    }
     
     ## Load knitcitations with a clean bibliography
     cleanbib()
@@ -283,33 +298,33 @@ derfinderReport <- function(prefix, outdir = 'basicExploration',
     
     ## knitrBoostrap chunk options
     opts_chunk$set(bootstrap.show.code = FALSE)
-    
+
     ## Generate report
+    ## Perform code within the output directory.
     tmpdir <- getwd()
-    with_wd(file.path(prefix, outdir), {
-    file.copy(template, to = paste0(output, '.Rmd'))
+    with_wd(outdir, {
+        file.copy(template, to = paste0(output, '.Rmd'))
     
-    ## Output format
-    output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
-    outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
+        ## Output format
+            output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
+        outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
     
-    ## Check knitrBoostrap version
-    knitrBootstrapFlag <- packageVersion('knitrBootstrap') < '1.0.0'
-    if(knitrBootstrapFlag & output_format == 'knitrBootstrap::bootstrap_document') {
-        ## CRAN version
-        tmp <- res <- knit_bootstrap(paste0(output, '.Rmd'), chooser = c('boot',
-            'code'), show_code = TRUE)
-        res <- file.path(tmpdir, outdir, paste0(output, '.html'))
-        unlink(paste0(output, '.md'))
-    } else {
-        res <- render(paste0(output, '.Rmd'), output_format,
-            clean = .advanced_argument('clean', TRUE, ...))
-    }
-    file.remove(paste0(output, '.Rmd'))
+        ## Check knitrBoostrap version
+        knitrBootstrapFlag <- packageVersion('knitrBootstrap') < '1.0.0'
+            if(knitrBootstrapFlag & output_format == 'knitrBootstrap::bootstrap_document') {
+            ## CRAN version
+            tmp <- knit_bootstrap(paste0(output, '.Rmd'), chooser = c('boot',
+                'code'), show_code = TRUE)
+            res <- file.path(tmpdir, outdir, paste0(output, '.html'))
+            unlink(paste0(output, '.md'))
+        } else {
+            res <- render(paste0(output, '.Rmd'), output_format,
+                clean = .advanced_argument('clean', TRUE, ...))
+        }
+        file.remove(paste0(output, '.Rmd'))
     
-    ## Open
-    if (browse) 
-        browseURL(res)
+        ## Open
+        if (browse) browseURL(res)
     })
     
     ## Finish

R/renderReport.R
@@ -222,9 +222,10 @@ This plot shows the density of the {{{densityVarName}}} for all regions. `r ifel
     
     ## Locate Rmd if one is not provided
     if (is.null(template)) {
-        template <- system.file(file.path('regionExploration',
-                                          'regionExploration.Rmd'),
-                                package = 'regionReport', mustWork = TRUE)
+        template <- system.file(
+            file.path('regionExploration', 'regionExploration.Rmd'),
+            package = 'regionReport', mustWork = TRUE
+        )
     }
     
     ## Load knitcitations with a clean bibliography
@@ -263,34 +264,31 @@ This plot shows the density of the {{{densityVarName}}} for all regions. `r ifel
     opts_chunk$set(bootstrap.show.code = FALSE)
     
     ## Generate report
-    ## Perform code within the output directory. (The contents of {} are not
-    ## indented, to minimize the footprint of the pull request, though they
-    ## probably should be)
+    ## Perform code within the output directory.
     tmpdir <- getwd()
     with_wd(outdir, {
-    file.copy(template, to = paste0(output, '.Rmd'))
+        file.copy(template, to = paste0(output, '.Rmd'))
     
-    ## Output format
-    output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
-    outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
+        ## Output format
+            output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
+        outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
     
-    ## Check knitrBoostrap version
-    knitrBootstrapFlag <- packageVersion('knitrBootstrap') < '1.0.0'
-    if(knitrBootstrapFlag & output_format == 'knitrBootstrap::bootstrap_document') {
-        ## CRAN version
-        tmp <- knit_bootstrap(paste0(output, '.Rmd'), chooser = c('boot',
-            'code'), show_code = TRUE)
-        res <- file.path(tmpdir, outdir, paste0(output, '.html'))
-        unlink(paste0(output, '.md'))
-    } else {
-        res <- render(paste0(output, '.Rmd'), output_format,
-            clean = .advanced_argument('clean', TRUE, ...))
-    }
-    file.remove(paste0(output, '.Rmd'))
+        ## Check knitrBoostrap version
+        knitrBootstrapFlag <- packageVersion('knitrBootstrap') < '1.0.0'
+            if(knitrBootstrapFlag & output_format == 'knitrBootstrap::bootstrap_document') {
+            ## CRAN version
+            tmp <- knit_bootstrap(paste0(output, '.Rmd'), chooser = c('boot',
+                'code'), show_code = TRUE)
+            res <- file.path(tmpdir, outdir, paste0(output, '.html'))
+            unlink(paste0(output, '.md'))
+        } else {
+            res <- render(paste0(output, '.Rmd'), output_format,
+                clean = .advanced_argument('clean', TRUE, ...))
+        }
+        file.remove(paste0(output, '.Rmd'))
     
-    ## Open
-    if (browse) 
-        browseURL(res)
+        ## Open
+        if (browse) browseURL(res)
     })
     
     ## Finish

DESCRIPTION
@@ -1,8 +1,8 @@
 Package: regionReport
 Type: Package
 Title: Generate HTML reports for exploring a set of regions
-Version: 1.3.4
-Date: 2015-06-15
+Version: 1.3.5
+Date: 2015-06-30
 Authors@R: c(person("Leonardo", "Collado-Torres", role = c("aut", "cre"), email
     = "lcollado@jhu.edu"), person(c("Andrew", "E."), "Jaffe", role = "aut",
     email = "andrew.jaffe@libd.org"), person(c("Jeffrey", "T."), "Leek",

R/utils.R
@@ -13,7 +13,7 @@
 }
 
 
-#' temporarily evaluate an expression in a directory
+#' Temporarily evaluate an expression in a directory
 #'
 #' Temporarily evaluate an expression in a directory, then set the directory
 #' back to the original.
@@ -22,6 +22,8 @@
 #' @param expr expression to evaluate
 #'
 #' @details See here: http://plantarum.ca/code/setwd-part2/
+#' @author Tyler Smith, contributed to regionReport by David Robinson 
+#' https://github.com/dgrtwo
 with_wd <- function(dir, expr) {
     wd <- getwd()
     on.exit(setwd(wd))

inst/CITATION
@@ -13,7 +13,7 @@ c(
         ),
         year = 2015,
         url = "http://www.bioconductor.org/packages/release/bioc/html/regionReport.html",
-        note = pkgVer('regionReport', '1.3.4')
+        note = pkgVer('regionReport', '1.3.5')
     ),
     bibentry(bibtype="article",
             title = "derfinder: Software for annotation-agnostic RNA-seq differential expression analysis",

inst/basicExploration/basicExploration.Rmd
@@ -630,7 +630,8 @@ $(document).ready(function() {
 ')
 ```
 
-
+```{r child = customCode, eval = hasCustomCode}
+```
 
 # Reproducibility
 

man/derfinderReport.Rd
@@ -8,7 +8,8 @@ derfinderReport(prefix, outdir = "basicExploration",
   output = "basicExploration", project = prefix, browse = interactive(),
   nBestRegions = 100, makeBestClusters = TRUE, nBestClusters = 2,
   fullCov = NULL, hg19 = TRUE, p.ideos = NULL, txdb = NULL,
-  device = "CairoPNG", significantVar = "qvalue", ...)
+  device = "CairoPNG", significantVar = "qvalue", customCode = NULL,
+  template = NULL, ...)
 }
 \arguments{
 \item{prefix}{The main data directory path where
@@ -56,6 +57,15 @@ http://yihui.name/knitr/options (\code{dev} argument).}
 p-values, the FDR adjusted p-values or the FWER adjusted p-values to
 determine significance. Has to be either \code{'pvalue'}, \code{'qvalue'}
 or \code{'fwer'}.}
+
+\item{customCode}{An absolute path to a child R Markdown file with code to be
+evaluated before the reproducibility section. Its useful for users who want
+to customize the report by adding conclusions derived from the data and/or
+further quality checks and plots.}
+
+\item{template}{Template file to use for the report. If not provided, will
+use the default file found in basicExploration/basicExploration.Rmd
+within the package source.}
 
 \item{...}{Arguments passed to other methods and/or advanced arguments.}
 }

man/with_wd.Rd
@@ -2,7 +2,7 @@
 % Please edit documentation in R/utils.R
 \name{with_wd}
 \alias{with_wd}
-\title{temporarily evaluate an expression in a directory}
+\title{Temporarily evaluate an expression in a directory}
 \usage{
 with_wd(dir, expr)
 }
@@ -17,5 +17,9 @@ back to the original.
 }
 \details{
 See here: http://plantarum.ca/code/setwd-part2/
+}
+\author{
+Tyler Smith, contributed to regionReport by David Robinson
+https://github.com/dgrtwo
 }
 

commit d228c95848c9ed57d24f621a94420325fadc63c0

Fix a bug introduced by https://github.com/leekgroup/regionReport/commit/f7a4d66a2279f771d30da23d3f8d22428ece1484

R/derfinderReport.R
@@ -302,11 +302,11 @@ derfinderReport <- function(prefix, outdir = 'basicExploration',
     ## Generate report
     ## Perform code within the output directory.
     tmpdir <- getwd()
-    with_wd(outdir, {
+    with_wd(file.path(prefix, outdir), {
         file.copy(template, to = paste0(output, '.Rmd'))
     
         ## Output format
-            output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
+        output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
         outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
     
         ## Check knitrBoostrap version

R/renderReport.R
@@ -220,7 +220,7 @@ renderReport <- function(regions, project, pvalueVars = c('P-values' = 'pval'),
         file.copy(template, to = paste0(output, '.Rmd'))
     
         ## Output format
-            output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
+        output_format <- .advanced_argument('output_format', 'knitrBootstrap::bootstrap_document', ...)
         outputIsHTML <- output_format %in% c('knitrBootstrap::bootstrap_document', 'html_document')
     
         ## Check knitrBoostrap version
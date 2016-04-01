167406da1d544df06b1a149c1cf511083616589e
Leonardo Collado-Torres <lcollado@jhu.edu>
2016-04-01 02:15:45 +0000

    v1.5.19 -- added histogram templates
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/regionReport@115571 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/renderReport.R
+++ R/renderReport.R
@@ -46,7 +46,9 @@
 #' are processed by \link[whisker]{whisker.render}. Check the default templates 
 #' for more information. The \code{densityTemplates} argument is available for 
 #' those users interested in customizing these plots. For example, to show 
-#' histograms instead of density plots.
+#' histograms instead of density plots use \code{templatePvalueHistogram} and
+#' \code{templateHistogram} instead of \code{templatePvalueDensity} and
+#' \code{templateDensity} respectively.
 #' @param template Template file to use for the report. If not provided, will
 #' use the default file found in regionExploration/regionExploration.Rmd
 #' within the package source.
@@ -358,5 +360,81 @@ rm(regions.manhattan)
 ```
 
 This is a Manhattan plot for the {{{densityVarName}}} for all regions. A single dot is shown for each region, where higher values in the y-axis mean that the {{{densityVarName}}} are closer to zero.
+
+"
+
+#' @rdname renderReport
+#' @export
+templatePvalueHistogram <- "
+## {{{densityVarName}}}
+
+```{r histPval-{{{varName}}}, fig.width=10, fig.height=10, dev=device, warning=FALSE}
+p1{{{varName}}} <- ggplot(regions.df.plot, aes(x={{{varName}}}, colour=seqnames)) +
+    geom_histogram(bins = 50, alpha=.5, position='identity') +
+    xlim(c(0, 1.0005)) +
+    labs(title='Histogram of {{{densityVarName}}}') + 
+    xlab('{{{densityVarName}}}') +
+    scale_colour_discrete(limits=chrs) + theme(legend.title=element_blank())
+p1{{{varName}}}
+```
+
+This plot shows the distribution of {{{densityVarName}}} in a histogram. It might be skewed right or left, or flat as shown in the [Wikipedia examples](https://en.wikipedia.org/wiki/Histogram#Examples). The shape depends on the percent of regions that are differentially expressed. For further information on how to interpret a histogram of p-values check [David Robinson's post on this topic](http://varianceexplained.org/statistics/interpreting-pvalue-histogram/).
+
+
+```{r 'summary-{{{varName}}}'}
+summary(mcols(regions)[['{{{varName}}}']])
+```
+
+
+This is the numerical summary of the distribution of the {{{densityVarName}}}.
+
+```{r tableSummary-{{{varName}}}, results='asis'}
+{{{varName}}}table <- lapply(c(1e-04, 0.001, 0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5,
+    0.6, 0.7, 0.8, 0.9, 1), function(x) {
+    data.frame('Cut' = x, 'Count' = sum(mcols(regions)[['{{{varName}}}']] <= x))
+})
+{{{varName}}}table <- do.call(rbind, {{{varName}}}table)
+if(outputIsHTML) {
+    kable({{{varName}}}table, format = 'markdown', align = c('c', 'c'))
+} else {
+    kable({{{varName}}}table)
+}
+```
+
+This table shows the number of regions with {{{densityVarName}}} less or equal than some commonly used cutoff values. 
+
+"
+
+#' @rdname renderReport
+#' @export
+templateHistogram <- "
+
+## {{{densityVarName}}}
+
+```{r histogram-{{{varName}}}, fig.width=14, fig.height=14, dev=device, eval=hasSignificant, echo=hasSignificant, warning = FALSE}
+xrange <- range(regions.df.plot[, '{{{varName}}}'])
+p3a{{{varName}}} <- ggplot(regions.df.plot[is.finite(regions.df.plot[, '{{{varName}}}']), ], aes(x={{{varName}}}, fill=seqnames)) +
+    geom_histogram(alpha=.5, position='identity', bins = 50) +
+    labs(title='Histogram of {{{densityVarName}}}') +
+    xlab('{{{densityVarName}}}') +
+    xlim(xrange) + theme(legend.title=element_blank())
+p3b{{{varName}}} <- ggplot(regions.df.sig[is.finite(regions.df.sig[, '{{{varName}}}']), ], aes(x={{{varName}}}, fill=seqnames)) +
+    geom_histogram(alpha=.5, position='identity', bins = 50) +
+    labs(title='Histogram of {{{densityVarName}}} (significant only)') +
+    xlab('{{{densityVarName}}}') +
+    xlim(xrange) + theme(legend.title=element_blank())
+grid.arrange(p3a{{{varName}}}, p3b{{{varName}}})
+```
+
+```{r histogram-solo-{{{varName}}}, fig.width=10, fig.height=10, dev=device, eval=!hasSignificant, echo=!hasSignificant, warning = FALSE}
+p3a{{{varName}}} <- ggplot(regions.df.plot[is.finite(regions.df.plot[, '{{{varName}}}']), ], aes(x={{{varName}}}, fill=seqnames)) +
+    geom_histogram(alpha=.5, position='identity', bins = 50) +
+    labs(title='Histogram of {{{densityVarName}}}') +
+    xlab('{{{densityVarName}}}') +
+    theme(legend.title=element_blank())
+p3a{{{varName}}}
+```
+
+This plot shows the distribution of {{{densityVarName}}} in a histogram for all regions. `r ifelse(hasSignificant, 'The bottom panel is restricted to significant regions.', '')`
 
 "

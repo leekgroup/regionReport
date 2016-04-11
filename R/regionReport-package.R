bece540f290770bd0461206ae49f504f98511b0e
lcolladotor <lcollado@jhsph.edu>
2016-04-10 22:46:53 -0400

    v1.5.37 -- add edgeR to description, update pkg man page

--- R/regionReport-package.R
+++ R/regionReport-package.R
@@ -1,9 +1,9 @@
-#' Generate HTML or PDF reports for a set of regions or DESeq2 results.
+#' Generate HTML or PDF reports for a set of regions or DESeq2/edgeR results.
 #'
 #' Generate an HTML reports to explore a set of regions such as the
 #' results from annotation-agnostic expression analysis of RNA-seq data at 
 #' base-pair resolution performed by derfinder. The package can also be used
-#' to generate reports for DESeq2 results.
+#' to generate reports for DESeq2 or edgeR results.
 #' The HTML and PDF reports are generated using rmarkdown 
 #' (http://rmarkdown.rstudio.com/).
 #'
